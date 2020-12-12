da80556f981b72bfb2c3594dd5ba16b3c21d74ee
Jukka Aho <ahojukka5@gmail.com>
2020-12-13 05:16:34 +0700

    Fix import of weathetRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import weatherRouter from './controllers/weather';
+import weatherRouter from './controllers/weather.js';
 
 const app = express();
 app.use(express.json());
