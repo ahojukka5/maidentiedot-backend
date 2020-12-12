8cf8609a5c1b970bc7f3ec9999bd684963fb411d
Jukka Aho <ahojukka5@gmail.com>
2020-12-13 05:03:34 +0700

    Add controllers/weather.js

---
+++ src/controllers/weather.js
@@ -0,0 +1,30 @@
+const http = require('http');
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  const options = {
+    host: 'api.weatherstack.com',
+    path: '/current?access_key=66fc4921f8cfa66fdbdbb69478498de0&query=Helsinki',
+  };
+
+  const callback = (response) => {
+    var str = '';
+
+    //another chunk of data has been received, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been received, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+      res.send(str);
+    });
+  };
+
+  http.request(options, callback).end();
+});
+
+export default router;
